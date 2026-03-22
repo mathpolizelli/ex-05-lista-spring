f426bad9aecd3946c4e313d2e6253ba79d77cbdf
Matheus Polizelli <matheuspolizelli@gmail.com>
2026-03-22 17:38:30 -0300

    categoria repository

---
+++ src/main/java/com/mep/categorias/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.mep.categorias.repositories;
+
+import com.mep.categorias.models.CategoriaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+}
